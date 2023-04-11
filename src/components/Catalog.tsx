35aa24156514a9c26a8d07e9b13fca2cc6ddcbae
IrvinFavors <irvin.favors@gmail.com>
2023-04-11 17:40:39 -0400

    finished adding description view when hovering over image

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -11,11 +11,13 @@ const Catalog: React.FC<CatalogProps> = ({ product }) => {
     return (
         <div className="catalog-view">
             <h1 className="name">{product.name}</h1>
-            <img
-                src={process.env.PUBLIC_URL + product.image}
-                alt={product.name}
-            />
-            <p className="img-description">`${}`</p>
+            <div className="image-container">
+                <img
+                    src={process.env.PUBLIC_URL + product.image}
+                    alt={product.name}
+                />
+                <div className="text-overlay">{product.description}</div>
+            </div>
             <p>In Stock: {product.units_instock}</p>
             <Button className="catalog-addToCart">Add to Cart</Button>
             <p>${product.price}</p>
