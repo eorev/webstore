5aa2aaae5bc3ee3088ee111e03b42d55f04dba18
IrvinFavors <irvin.favors@gmail.com>
2023-04-12 19:39:39 -0400

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
-// You will not need to modify this file
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCr1YE2l3qCNRIerh-8jhdg7HnKhSqilEM",
+    authDomain: "team-5-webstore.firebaseapp.com",
+    projectId: "team-5-webstore",
+    storageBucket: "team-5-webstore.appspot.com",
+    messagingSenderId: "620195896772",
+    appId: "1:620195896772:web:4d4ec3521bcc2556c21891",
+    measurementId: "G-7VWHRTGJ06"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
 
 ReactDOM.render(
     <React.StrictMode>
