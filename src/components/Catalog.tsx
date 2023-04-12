22ef1f0be8d6c62484d032a34ffc4f955e4da9a3
IrvinFavors <irvin.favors@gmail.com>
2023-04-12 13:53:22 -0400

    added ability to add products; products array may need to be changed to tsx file instead of JSON for future changes

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductData from "../interfaces/product";
 import { Button } from "react-bootstrap";
 import "./Catalog.css";
-import inventory from "../data/products.json";
+import inventoryData from "../data/products";
 
 interface CatalogProps {
     product: ProductData;
@@ -49,13 +49,137 @@ const View: React.FC<CatalogProps> = ({ product }) => {
 };
 
 export default function Catalog() {
+    const [inventory, setInventory] = useState<ProductData[]>([]);
+    const [showForm, setShowForm] = useState(false);
+    const [newProduct, setNewProduct] = useState<ProductData>({
+        name: "",
+        description: "",
+        id: 0,
+        image: "",
+        rating: 0,
+        category: "",
+        admin_id: 0,
+        price: 0,
+        units_instock: 0
+    });
+    useEffect(() => {
+        const inventoryJSON = sessionStorage.getItem("inventory");
+        if (inventoryJSON) {
+            setInventory(JSON.parse(inventoryJSON));
+        } else {
+            const inventoryDataJSON = JSON.stringify(inventoryData);
+            sessionStorage.setItem("inventory", inventoryDataJSON);
+            setInventory(inventoryData);
+        }
+    }, []);
+
+    const addProduct = () => {
+        setInventory([...inventory, newProduct]);
+        setShowForm(false);
+    };
+
+    const toggleForm = () => {
+        setShowForm(!showForm);
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setNewProduct({ ...newProduct, [name]: value });
+    };
+
     return (
-        <div className="catalog-container">
-            {inventory.PRODUCTS.map((item: ProductData) => (
+        <div className="catalog-container" id="container">
+            {inventory.map((item: ProductData) => (
                 <div key={item.name} className="catalog-item">
                     <View product={item} />
                 </div>
             ))}
+            <div>
+                <Button
+                    onClick={toggleForm}
+                    className="catalog-add-product-button"
+                    id="AddProduct"
+                >
+                    Add Product
+                </Button>
+            </div>
+            {showForm && (
+                <div className="catalog-add-product-container">
+                    {
+                        <form onSubmit={addProduct}>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Product Name"
+                                value={newProduct.name}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="description"
+                                placeholder="Product Description"
+                                value={newProduct.description}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="number"
+                                name="id"
+                                placeholder="Product ID"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="image"
+                                placeholder="Product Image URL"
+                                value={newProduct.image}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="number"
+                                name="rating"
+                                placeholder="Product Rating"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="category"
+                                placeholder="Product Category"
+                                value={newProduct.category}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="number"
+                                name="admin_id"
+                                placeholder="Admin ID"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="number"
+                                name="price"
+                                placeholder="Product Price"
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="number"
+                                name="units_instock"
+                                placeholder="Units in Stock"
+                                onChange={handleInputChange}
+                            />
+                            <Button
+                                type="submit"
+                                className="catalog-button"
+                                style={{
+                                    backgroundColor: "black",
+                                    position: "absolute",
+                                    bottom: -12
+                                }}
+                            >
+                                Add
+                            </Button>
+                        </form>
+                    }
+                </div>
+            )}
         </div>
     );
 }
