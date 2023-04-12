b3d99ddbe91c1033bdd4dff7a39f46c571acaff9
IrvinFavors <irvin.favors@gmail.com>
2023-04-11 22:14:21 -0400

    added function call for Catalog in App.tsx rather than direct calls; added state to handle addToCart->unitsInStick

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import inventory from "./data/products.json";
 import Catalog from "./components/Catalog";
 import "./components/Catalog.css";
-import product from "./interfaces/product";
 
 function App() {
     return (
         <div className="App">
             <Navbar></Navbar>
-            <div className="catalog-container">
-                {inventory.PRODUCTS.map((item: product) => (
-                    <div key={item.name} className="catalog-item">
-                        <Catalog product={item} />
-                    </div>
-                ))}
-            </div>
+            <Catalog></Catalog>
         </div>
     );
 }
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductData from "../interfaces/product";
 import { Button } from "react-bootstrap";
 import "./Catalog.css";
+import inventory from "../data/products.json";
 
 interface CatalogProps {
     product: ProductData;
 }
 
-const Catalog: React.FC<CatalogProps> = ({ product }) => {
+const View: React.FC<CatalogProps> = ({ product }) => {
+    const [productUnits, setProductUnits] = useState(0);
+
+    const handleAddToCart = () => {
+        if (product.units_instock > 0) {
+            // Decrease the units in stock by 1 and increase the units in cart by 1
+            product.units_instock--;
+            setProductUnits(productUnits + 1);
+        }
+    };
+
     return (
         <div className="catalog-view">
             <h1 className="name">{product.name}</h1>
@@ -18,11 +29,33 @@ const Catalog: React.FC<CatalogProps> = ({ product }) => {
                 />
                 <div className="text-overlay">{product.description}</div>
             </div>
-            <p>In Stock: {product.units_instock}</p>
-            <Button className="catalog-addToCart">Add to Cart</Button>
-            <p>${product.price}</p>
+            {product.units_instock <= 0 ? (
+                <p>Out of Stock</p>
+            ) : (
+                <div className="catalog-view">
+                    {" "}
+                    <p>In Stock: {product.units_instock}</p>
+                    <Button
+                        className="catalog-addToCart"
+                        onClick={handleAddToCart}
+                    >
+                        Add to Cart
+                    </Button>
+                    <p>${product.price}</p>
+                </div>
+            )}
         </div>
     );
 };
 
-export default Catalog;
+export default function Catalog() {
+    return (
+        <div className="catalog-container">
+            {inventory.PRODUCTS.map((item: ProductData) => (
+                <div key={item.name} className="catalog-item">
+                    <View product={item} />
+                </div>
+            ))}
+        </div>
+    );
+}
