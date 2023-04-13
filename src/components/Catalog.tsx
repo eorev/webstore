6723876a6d3cd73ecc767929aa64d185cb35b620
IrvinFavors <irvin.favors@gmail.com>
2023-04-13 01:14:55 -0400

    removed errors that would cause linter errors

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import ProductData from "../interfaces/product";
 import { Button } from "react-bootstrap";
