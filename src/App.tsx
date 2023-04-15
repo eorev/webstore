9278a41956ab5028578fa8a8f295f5ccc6ed2b8a
eorev <eorev@udel.edu>
2023-04-15 12:51:03 -0700

    Finished Navbar, working links, logo styled

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import catalog from "./data/products.json";
 function App() {
     return (
         <div className="App">
-            <Navbar></Navbar>
+            <Navbar
+                links={[
+                    { name: "About", url: "#about" },
+                    { name: "Products", url: "#products" }
+                ]}
+                cartOnClick={() => {
+                    console.log("Open Cart");
+                }}
+            />
             <span>{catalog.PRODUCTS[0].name}</span>
         </div>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "./Navbar.css";
 
-export default function Navbar() {
+type NavbarProps = {
+    links: { name: string; url: string }[];
+    cartOnClick: () => void;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ links, cartOnClick }) => {
     return (
-        <nav>
-            <div className="logo">Store</div>
+        <nav className="navbar">
+            <div className="navbar__logo">
+                <h1>Nestled</h1>
+            </div>
+            <ul className="navbar__links">
+                {links.map((link) => (
+                    <li key={link.url} className="navbar__links-item">
+                        <a href={link.url}>{link.name}</a>
+                    </li>
+                ))}
+                <button
+                    className="navbar__cart-btn navbar__links-item"
+                    onClick={cartOnClick}
+                >
+                    Cart
+                </button>
+            </ul>
         </nav>
     );
-}
+};
+
+export default Navbar;
