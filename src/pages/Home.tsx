23af2230f463abf03104a7b951ed22958d3f60d1
IrvinFavors <irvin.favors@gmail.com>
2023-04-15 15:03:36 -0400

    fixing broken page attempt#1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Catalog from "../components/Catalog";
 
 const Home = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Navbar
+                links={[
+                    { name: "About", url: "/about" },
+                    { name: "Products", url: "#products" }
+                ]}
+                cartOnClick={() => {
+                    console.log("Open Cart");
+                }}
+            />
+            <Catalog></Catalog>
+        </div>
+    );
 };
 
 export default Home;
