ebd0f7afdbd91a0fa56880e7ea8bcfda6b4e8c5d
IrvinFavors <irvin.favors@gmail.com>
2023-04-15 14:56:01 -0400

    fixing blank page attempt #3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,7 @@ import Navbar from "../components/Navbar";
 import Catalog from "../components/Catalog";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar
-                links={[
-                    { name: "About", url: "/about" },
-                    { name: "Products", url: "#products" }
-                ]}
-                cartOnClick={() => {
-                    console.log("Open Cart");
-                }}
-            />
-            <Catalog></Catalog>
-        </div>
-    );
+    return <div></div>;
 };
 
 export default Home;
