abecf97bc6b3000139e4d9b4718dab1b4a39da02
eorev <eorev@udel.edu>
2023-04-19 17:38:44 -0700

    Added section for Featured In

---
+++ src/components/Features.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Features() {
+    return (
+        <div className="features">
+            <div className="features-top__container">
+                <h1 className="features-top__title">Featured In</h1>
+                <div className="features-top__logos">
+                    <img src="https://img.logoipsum.com/288.svg" alt="logo1" />
+                    <img src="https://img.logoipsum.com/288.svg" alt="logo2" />
+                    <img src="https://img.logoipsum.com/288.svg" alt="logo3" />
+                    <img src="https://img.logoipsum.com/288.svg" alt="logo4" />
+                    <img src="https://img.logoipsum.com/288.svg" alt="logo5" />
+                </div>
+            </div>
+            <div className="features-bottom__container">
+                <div className="features-bottom__reviews">
+                    <h1>4.8</h1>
+                    <ul className="stars">
+                        <li className="star">
+                            <i className="fas fa-star"></i>
+                        </li>
+                        <li className="star">
+                            <i className="fas fa-star"></i>
+                        </li>
+                        <li className="star">
+                            <i className="fas fa-star"></i>
+                        </li>
+                        <li className="star">
+                            <i className="fas fa-star"></i>
+                        </li>
+                        <li className="star">
+                            <i className="fas fa-star"></i>
+                        </li>
+                    </ul>
+                    <h2>2,321 Google Reviews</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import HotProducts from "../components/HotProducts";
+import Features from "../components/Features";
 
 const Home = () => {
     return (
         <div className="home">
             <Header></Header>
+            <Features></Features>
             <HotProducts></HotProducts>
         </div>
     );
