a3f5b5924842f567f232221ee98506b3136909eb
eorev <eorev@udel.edu>
2023-04-20 00:38:13 -0700

    Finished styling the features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,33 +8,59 @@ export default function Features() {
                 <h1 className="features-top__title">Featured In</h1>
                 <div className="features-top__logos">
                     <img src="https://img.logoipsum.com/288.svg" alt="logo1" />
-                    <img src="https://img.logoipsum.com/288.svg" alt="logo2" />
-                    <img src="https://img.logoipsum.com/288.svg" alt="logo3" />
-                    <img src="https://img.logoipsum.com/288.svg" alt="logo4" />
-                    <img src="https://img.logoipsum.com/288.svg" alt="logo5" />
+                    <img src="https://img.logoipsum.com/286.svg" alt="logo2" />
+                    <img src="https://img.logoipsum.com/285.svg" alt="logo3" />
+                    <img src="https://img.logoipsum.com/219.svg" alt="logo4" />
+                    <img src="https://img.logoipsum.com/216.svg" alt="logo5" />
                 </div>
             </div>
             <div className="features-bottom__container">
-                <div className="features-bottom__reviews">
-                    <h1>4.8</h1>
-                    <ul className="stars">
-                        <li className="star">
-                            <i className="fas fa-star"></i>
-                        </li>
-                        <li className="star">
-                            <i className="fas fa-star"></i>
-                        </li>
-                        <li className="star">
-                            <i className="fas fa-star"></i>
-                        </li>
-                        <li className="star">
-                            <i className="fas fa-star"></i>
-                        </li>
-                        <li className="star">
-                            <i className="fas fa-star"></i>
-                        </li>
-                    </ul>
+                <div className="reviews-stars__container">
+                    <div className="star-rating__container">
+                        <h1>4.8</h1>
+                        <div className="stars">
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL + "/images/star.svg"
+                                }
+                                alt="star1"
+                            />
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL + "/images/star.svg"
+                                }
+                                alt="star2"
+                            />
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL + "/images/star.svg"
+                                }
+                                alt="star3"
+                            />
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL + "/images/star.svg"
+                                }
+                                alt="star4"
+                            />
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL + "/images/star.svg"
+                                }
+                                alt="star5"
+                            />
+                        </div>
+                    </div>
                     <h2>2,321 Google Reviews</h2>
+                </div>
+                <div className="features-bottom__reviews">
+                    <h1>Trusted by numerous Modern furniture enthusiasts.</h1>
+                    <h2>Jessica Simon</h2>
+                    <p>
+                        I absolutely love the modern furniture collection from
+                        Nestled, and their website made the purchasing process a
+                        breeze.
+                    </p>
                 </div>
             </div>
         </div>
