ce44283019222e2272a06bcded5226e20c7dff39
Charles Gordinier III <cwgiii@udel.edu>
2023-04-21 00:00:24 -0400

    added changes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <footer>
-            <h1>Footer</h1>
+            <h1>2023 Nestled inc.</h1>
+            <div className="footer__right">
+                <p>UD CISC275</p>
+            </div>
         </footer>
     );
 }
