e9e2db855c7f5a4a3dfe27df052d377e7628909a
IrvinFavors <irvin.favors@gmail.com>
2023-04-24 20:40:18 -0400

    added remove from cart and temporary cart display in account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import { User } from "firebase/auth";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import ProductData from "../interfaces/product";
+import db from "../firebase";
 
 interface AuthContextType {
     logout: () => void;
+    user: User;
 }
 
 const Account = () => {
-    const { logout } = UserAuth() as AuthContextType;
+    const { logout, user } = UserAuth() as AuthContextType;
+    const [removeProduct, setRemoveProduct] = useState<string>("");
+    const [products, setProducts] = useState<ProductData[]>([]);
+    console.log(products);
+    useEffect(
+        () =>
+            onSnapshot(
+                collection(db, "carts", user.uid, "products"),
+                (snapshot) =>
+                    setProducts(
+                        snapshot.docs.map((doc) => doc.data() as ProductData)
+                    )
+            ),
+        []
+    );
+    const handleRemove = async () => {
+        if (!user) {
+            const productRef = doc(
+                collection(db, "carts", "temp", "products"),
+                removeProduct
+            );
+            await deleteDoc(productRef);
+        }
+        if (user) {
+            const productRef = doc(
+                collection(db, "carts", user.uid, "products"),
+                removeProduct
+            );
+            await deleteDoc(productRef);
+        }
+    };
     return (
         <div>
             <p>Account</p>
@@ -15,6 +50,24 @@ const Account = () => {
                 <button>Home</button>
             </Link>
             <button onClick={logout}>Logout</button>
+            <div>
+                {products.map((item: ProductData) =>
+                    item.name !== "null" ? (
+                        <div key={item.name}>
+                            <span>{item.name}</span>
+                            <button
+                                onClick={() => {
+                                    setRemoveProduct(item.name), handleRemove();
+                                }}
+                            >
+                                Remove
+                            </button>
+                        </div>
+                    ) : (
+                        <div key={item.id}></div>
+                    )
+                )}
+            </div>
         </div>
     );
 };
