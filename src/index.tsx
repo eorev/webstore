254fb41d0fdba4165828f7ee966e442f7b355894
eorev <eorev@udel.edu>
2023-04-30 20:58:50 -0700

    Working rotating carousel somewhat styled

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import "react-multi-carousel/lib/styles.css";
 
 ReactDOM.render(
     <HashRouter>
