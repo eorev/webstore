d7833bbefe3fdf524dfc563d30304a3de7326588
IrvinFavors <irvin.favors@gmail.com>
2023-05-09 13:31:58 -0400

    users can now edit their email, password and displayname; need to disable for users using googleSignin

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
-import { User } from "firebase/auth";
-import { updateProfile } from "firebase/auth";
+import {
+    EmailAuthProvider,
+    User,
+    reauthenticateWithCredential
+} from "firebase/auth";
+import { updateProfile, updateEmail, updatePassword } from "firebase/auth";
 
 import "./Account.css";
 
@@ -11,6 +15,14 @@ interface AuthContextType {
 
 interface ChangeDisplayNameFormProps {
     onSubmit: (newDisplayName: string) => void;
+}
+
+interface ChangeEmailFormProps {
+    onSubmit: (newEmail: string, password: string) => void;
+}
+
+interface ChangePasswordFormProps {
+    onSubmit: (currentPassword: string, newPassword: string) => void;
 }
 
 function ChangeDisplayNameForm({ onSubmit }: ChangeDisplayNameFormProps) {
@@ -28,7 +40,7 @@ function ChangeDisplayNameForm({ onSubmit }: ChangeDisplayNameFormProps) {
     return (
         <form onSubmit={handleSubmit}>
             <label>
-                New display name:
+                New Display Name:
                 <input
                     type="text"
                     value={newDisplayName}
@@ -36,6 +48,128 @@ function ChangeDisplayNameForm({ onSubmit }: ChangeDisplayNameFormProps) {
                 />
             </label>
             <button type="submit">Update</button>
+        </form>
+    );
+}
+
+function ChangeEmailForm({ onSubmit }: ChangeEmailFormProps) {
+    const [newEmail, setNewEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onSubmit(newEmail, password);
+    };
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                New Email: <br />
+                <input
+                    type="email"
+                    value={newEmail}
+                    onChange={handleEmailChange}
+                />
+            </label>
+            <label>
+                Current password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </label>
+            <button type="submit">Update</button>
+        </form>
+    );
+}
+
+function ChangePasswordForm({ onSubmit }: ChangePasswordFormProps) {
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (newPassword !== confirmNewPassword) {
+            setPasswordsMatch(false);
+            return;
+        }
+        onSubmit(currentPassword, newPassword);
+    };
+
+    const handleCurrentPasswordChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setCurrentPassword(e.target.value);
+    };
+
+    const handleNewPasswordChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setNewPassword(e.target.value);
+        if (passwordsMatch === false && e.target.value === confirmNewPassword) {
+            setPasswordsMatch(true);
+        }
+    };
+
+    const handleConfirmNewPasswordChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setConfirmNewPassword(e.target.value);
+        if (passwordsMatch === false && e.target.value === newPassword) {
+            setPasswordsMatch(true);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Current password:
+                <input
+                    type="password"
+                    value={currentPassword}
+                    onChange={handleCurrentPasswordChange}
+                />
+            </label>
+            <label>
+                New password:
+                <input
+                    type="password"
+                    value={newPassword}
+                    onChange={handleNewPasswordChange}
+                />
+            </label>
+            <label>
+                Confirm new password:
+                <input
+                    type="password"
+                    value={confirmNewPassword}
+                    onChange={handleConfirmNewPasswordChange}
+                />
+                {!passwordsMatch && (
+                    <div style={{ textAlign: "center" }}>
+                        <div>
+                            <br />
+                            <span style={{ color: "red" }}>
+                                Passwords do not match.
+                            </span>
+                        </div>
+                    </div>
+                )}
+            </label>
+            <button type="submit" disabled={!passwordsMatch}>
+                Update
+            </button>
         </form>
     );
 }
@@ -54,6 +188,72 @@ async function updateDisplayName(user: User, newDisplayName: string) {
 const Account = () => {
     const { user } = UserAuth() as AuthContextType;
 
+    const handleEmailUpdate = async (newEmail: string, password: string) => {
+        try {
+            const email = user.email;
+            if (!email) {
+                console.error("User email not found");
+            } else {
+                const credential = EmailAuthProvider.credential(
+                    email,
+                    password
+                );
+                try {
+                    await reauthenticateWithCredential(user, credential);
+                } catch (error) {
+                    console.log(
+                        "^Error for Incorrect Password: Cant reauthenticate user with this password"
+                    );
+                    alert("Incorret input for Current Password");
+                    <span>Error</span>;
+                    return;
+                }
+                try {
+                    await updateEmail(user, newEmail);
+                } catch (error) {
+                    console.log("Error updating email");
+                }
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error("Error updating email:", error);
+        }
+    };
+
+    const handlePasswordUpdate = async (
+        currentPassword: string,
+        newPassword: string
+    ) => {
+        try {
+            const email = user.email;
+            if (!email) {
+                console.error("User email not found");
+                return;
+            }
+            const credential = EmailAuthProvider.credential(
+                user.email,
+                currentPassword
+            );
+            try {
+                await reauthenticateWithCredential(user, credential);
+            } catch (error) {
+                console.log(
+                    "^Error for Incorrect Password: Cant reauthenticate user with this password"
+                );
+                alert("Incorret input for Current Password");
+                return;
+            }
+            try {
+                await updatePassword(user, newPassword);
+            } catch (error) {
+                console.log("Error updating password");
+                return;
+            }
+            window.location.reload();
+        } catch (error) {
+            console.error("Error updating password:", error);
+        }
+    };
     return (
         <div>
             <h1>Account</h1>
@@ -63,6 +263,8 @@ const Account = () => {
                         updateDisplayName(user, newDisplayName);
                     }}
                 />
+                <ChangeEmailForm onSubmit={handleEmailUpdate} />
+                <ChangePasswordForm onSubmit={handlePasswordUpdate} />
             </div>
         </div>
     );
