6911dfb92095c345f55ae796c67bf4e05057d2eb
IrvinFavors <irvin.favors@gmail.com>
2023-05-12 13:08:25 -0400

    adding back checkout tests

--- src/pages/Checkout.test.tsx
+++ src/pages/Checkout.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Checkout from "./Checkout";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Checkout Process", () => {
     beforeEach(() => {
@@ -11,7 +11,57 @@ describe("Checkout Process", () => {
             </BrowserRouter>
         );
     });
-    test("renders", () => {
-        //
+    test("Place Order button exists", () => {
+        const placeOrderButton = screen.getByRole("button", {
+            name: /Place Order/i
+        });
+        expect(placeOrderButton).toBeInTheDocument();
+    });
+    test("Payment form renders on Add Payment Click", () => {
+        const addPaymentButton = screen.getByText("Add Payment");
+        fireEvent.click(addPaymentButton);
+
+        const paymentFormContainer = screen.getByTestId(
+            "payment-form-container"
+        );
+        expect(paymentFormContainer).toBeInTheDocument();
+    });
+    test("Shipping form renders on Add Shipping Click", () => {
+        const addShippingButton = screen.getByText("Add Shipping");
+        fireEvent.click(addShippingButton);
+
+        const shippingForm = screen.getByTestId("shipping-form");
+        expect(shippingForm).toBeInTheDocument();
+    });
+    test("Successful Order Placement", () => {
+        const placeOrderButton = screen.getByText("Place Order");
+
+        placeOrderButton.removeAttribute("disabled");
+        expect(placeOrderButton).toBeEnabled();
+
+        fireEvent.click(placeOrderButton);
+
+        setTimeout(() => {
+            const orderConfirmationScreen = screen.getByTestId("underlay");
+            expect(orderConfirmationScreen).toBeInTheDocument();
+        }, 2000);
+    });
+    test("renders a cart display", () => {
+        const cartDisplayContainer = screen.getByTestId(
+            "cart-display-container"
+        );
+        const productsContainer = screen.getByTestId("products-container");
+        expect(cartDisplayContainer).toBeInTheDocument();
+        expect(productsContainer).toBeInTheDocument();
+    });
+    test("renders shipping options", () => {
+        const standardShipping = screen.getByTestId("standard-shipping");
+        expect(standardShipping).toBeInTheDocument();
+
+        const expressShipping = screen.getByTestId("express-shipping");
+        expect(expressShipping).toBeInTheDocument();
+
+        const shippingLabel = screen.getByText("Shipping Options");
+        expect(shippingLabel).toBeInTheDocument();
     });
 });
