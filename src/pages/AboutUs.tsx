d5fc7315ca468e5dbde8e0d1b915013eb8e7d96e
Farhan Ohe <farhanto@udel.edu>
2024-05-16 00:24:50 -0400

    added names in about us, so I can create an instance on github

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -5,6 +5,7 @@ const AboutUs = () => {
     return (
         <div>
             <h1 className="aboutus-title">Meet our Team</h1>
+            <p>Farhan Ohe, Irvin Favors, Ethan Orovillo</p>
         </div>
     );
 };
